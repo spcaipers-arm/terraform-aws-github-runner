3452cf4bbab4f354860aa3bdfd52889f684e5b01
Samuel Pelegrinello Caipers <samuel.pelegrinellocaipers@arm.com>
2023-04-25 15:32:21 +0100

    Cleanup files

--- deployments/multi-runner/variables.tf
+++ deployments/multi-runner/variables.tf
@@ -1,11 +1,3 @@
-# variable "github_app" {
-#   description = "GitHub for API usages."
-#   type = object({
-#     id         = string
-#     key_base64 = string
-#   })
-# }
-
 variable "environment" {
   type    = string
   default = null
